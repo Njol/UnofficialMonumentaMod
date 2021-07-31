87a7646041ac8abb1ba93e1328f664935f140b71
Njol <njol@gmx.ch>
2021-07-31 12:42:13 +0200

    Added a fix for missing trident textures

---
+++ README.md
@@ -0,0 +1,21 @@
+This is an unofficial client-side mod for the Monumenta MMO server (www.playmonumenta.com).
+
+## Feature List
+
+### Firmament ping fix
+
+Makes the "Firmament" item usable even at high ping (and may also be an improvement even with low ping).
+
+### Trident texture fix
+
+Shows custom trident textures of the resource pack when tridents are held instead of only when in the inventory.
+
+**Important: The resource pack "Fabric Mods" needs to be above the Monumenta resource pack!** Otherwise, tridents will
+be tiny when held.
+
+The thrown trident projectiles still look like the vanilla trident, as changing that would require a server-side mod.
+
+## Contribute
+
+[The code is open source](https://github.com/Njol/UnofficialMonumentaMod) and pull requests for new features or fixes
+are always welcome!
---
+++ src/main/java/ch/njol/unofficialmonumentamod/mixins/ItemRendererMixin.java
@@ -0,0 +1,51 @@
+package ch.njol.unofficialmonumentamod.mixins;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.ItemModels;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ItemRenderer.class)
+public class ItemRendererMixin {
+
+	@Shadow
+	@Final
+	private ItemModels models;
+
+	/**
+	 * Pretend that tridents are apples so that the trident-specific code is not executed
+	 */
+	@Redirect(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"))
+	public Item renderItem_tridentFix1(ItemStack itemStack) {
+		Item item = itemStack.getItem();
+		if (item == Items.TRIDENT)
+			return Items.APPLE;
+		return item;
+	}
+
+	/**
+	 * Always use the trident item model instead of the entity model
+	 */
+	@ModifyVariable(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+			at = @At("HEAD"),
+			ordinal = 0)
+	public BakedModel renderItem_tridentFix2(BakedModel model, ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model2) {
+		if (stack.getItem() == Items.TRIDENT)
+			return models.getModelManager().getModel(new ModelIdentifier("minecraft:trident#inventory"));
+		return model;
+	}
+
+}
