84952de69a1bb5b7b6b9e0b68822666887e6dc87
Ascynx <miaoum2128@gmail.com>
2022-06-24 08:36:19 +0200

    Added DiscordRPC support for 1.17
    I don't know what to say here ngl

---
+++ src/main/java/ch/njol/unofficialmonumentamod/mixins/PlayerListHudAccessor.java
@@ -0,0 +1,12 @@
+package ch.njol.unofficialmonumentamod.mixins;
+
+import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PlayerListHud.class)
+public interface PlayerListHudAccessor {
+    @Accessor("header")
+    Text getHeader();
+}
